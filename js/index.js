73d143e53c2b5fa4c42965763f11e0952605fa85
aliengamer38 <aliengamer38@gmail.com>
2021-02-11 07:17:43 +0800

    news

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const topHeader = $("#top-header");
+let currentScrollPos = 0;
+let lastScrollPos = 0;
+
+$(window).scroll(function (e) {
+    currentScrollPos = (this).scrollTop();
+    if (currentScrollPos > lastScrollPos) {
+        topHeader.
+    }    
+})
+
