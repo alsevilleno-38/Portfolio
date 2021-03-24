afa2caa704ff8ace6915244f69e4ebf6097b5239
aliengamer38 <aliengamer38@gmail.com>
2021-03-24 13:37:20 +0800

    ok

--- public/js/testing/auto.js
+++ public/js/testing/auto.js
@@ -1,5 +1,14 @@
-$("#btn").on("click", function (e) {
-    // console.log("test");
-    $(".box").toggleClass("active");
-    // alert("test");
+let section1 = $("#section-1");
+
+$(document).scroll(function () {        
+    if ($(this).scrollTop() >= section1.offset().top - 90 && !section1.stopDisplay) {
+        section1.addClass("add");
+    }
+    else {
+        section1.removeClass("add");        
+    }
+})
+
+$("section").each((index, element) => {
+
 })
